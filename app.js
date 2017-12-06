0de45ca1dea36e0c0b620190f3b08b7f847cc12e
Sh3nr0n <mehdi.lefeuvre@gmail.com>
2017-12-06 12:17:28 +0100

    upload files

--- app.js
+++ app.js
@@ -47,13 +47,70 @@
 // }
 
 
-console.log ("l'éléphant")
+console.log ("l'éléphant");
 
 function camelCase(id) {
     // console.log (id);
     // console.log (document.getElementById(id).value);
-    var nombre=document.getElementById(id).value
-    console.log ("nombre:",nombre)
+    //va chercher le chiffre entré par l'utilisateur et le stocke dans la variable nombre
+    var nombre=document.getElementById(id).value;
+  
+    console.log ("nombre:",nombre);
+    //On ajoute nombre à la valeur de calc_resultat pour afficher les chiffres tapés à la calculatrices les uns à la suite des autres
+    document.getElementById("calc_resultat").value= calc_resultat.value + nombre;
 
 }
+
+// fonction clear/supprimer
+function supprimer() {
+    document.getElementById("calc_resultat").value ="";
+    
+}
+
+
+//on fixe les états initiaux des variables a et b
+var a = 0; 
+
+var b = 0;
+
+//a= a + b;
+
+
+//on créé une fonction addition qui renvoie le résultat des arguments a et b
+function addition(a,b) {
+    console.log(b);
+    a = document.getElementById("calc_resultat").value;
+    b = parseInt(a) + parseInt(b);
+
+    //console.log ("a :", parseInt(a));
+    //console.log ("b :", parseInt(b));
+    //document.getElementById("calc_resultat").value = b;
+    console.log("addition :" + b)
+    return b
+    
+}
+
+
+
+
+
+
+//stocker les nombres et opérations de l'utilisateur
+
+/* - une   fonction   nommée   "addition"   qui   renvoie   le   résultat  
+ de   l'addition   des  deux​ ​ arguments​ ​ a ​ ​ et​ ​ b. 
+- une   fonction   nommée   "soustraction"   qui   renvoie   le   résultat 
+  de   la  soustraction​ ​ des​ ​ deux​ ​ arguments​ ​ a ​ ​ et​ ​ b. 
+- une   fonction   nommée   "multiplication"   qui   renvoie   le   résultat  
+ de   la  multiplication​ ​ des​ ​ deux​ ​ arguments​ ​ a ​ ​ et​ ​ b. 
+- une​ ​ fonction​ ​ nommée​ ​ "division": 
+- Si​ ​ b ​ ​ > ​ ​ 0 ​ ​ qui​ ​ renvoie​ ​ le​ ​ résultat​ ​ de​ ​ la​ ​ division​ ​ a/b. 
+- Si   b   =<   0   qui   renvoie   le   message   d'erreur   
+suivant   :   "Impossible   de  diviser​ ​ par​ ​ 0" */
+
+
+
+
+
+
 
